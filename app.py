74e8f03626ac8c797baa95469ba3c2207f7d2102
jakechevrier <jakechevrier@gmail.com>
2024-05-19 23:41:28 -0700

    model to cloud

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from uuid import uuid4
 import streamlit as st
 import honeybee
 import ladybug
+from ladybug.epw import EPW
+from ladybug.wea import Wea
 from honeybee.room import Room
 from honeybee.model import Model, Face
 from honeybee_radiance.properties.model import ModelRadianceProperties
@@ -13,23 +15,21 @@ from honeybee_vtk.vtkjs.schema import SensorGridOptions
 from honeybee_radiance.sensorgrid import SensorGrid
 from visualize_model import generate_vtk_model
 from streamlit_vtkjs import st_vtkjs
-from query import Query
-from pollination_streamlit.api.client import ApiClient
-from pollination_streamlit.interactors import Job, NewJob, Recipe
+from pollination_io.api.client import ApiClient
+from pollination_io.interactors import Job, NewJob, Recipe
 from queenbee.job.job import JobStatusEnum
+from streamlit_autorefresh import st_autorefresh
 
-
-query = Query()
 
 api_key = st.sidebar.text_input(
-    'Enter Pollination APIKEY', type='password',
+    'Enter Pollination APIKEY', type='password', value='8B8AC8B0.13F7476390ECDE0F50F7ED05',
     help=':bulb: You only need an API Key to access private projects. '
     'If you do not have a key already go to the settings tab under your profile to '
     'generate one.'
 ) or None
 
-query.owner = st.sidebar.text_input('Project Owner', value=query.owner)
-query.project = st.sidebar.text_input('Project Name', value=query.project)
+owner = st.sidebar.text_input('Project Owner', value="justinshultz")
+project = st.sidebar.text_input('Project Name', value="hacksimbuild-2024")
 
 run_simulation = st.sidebar.button('Run Simulation')
 api_client = ApiClient(api_token=api_key)
@@ -76,9 +76,9 @@ simple_model._properties._radiance = ModelRadianceProperties(simple_model, [grid
 model_path = simple_model.to_hbjson(name=simple_model.identifier, folder='data')
 vtk_path = pathlib.Path('data', f'{simple_model.identifier}.vtkjs')
 
-# if not vtk_path.is_file():
-VTKModel.from_hbjson(model_path, SensorGridOptions.Sensors).to_vtkjs(
-    folder='data', name=simple_model.identifier)
+if not vtk_path.is_file():
+    VTKModel.from_hbjson(model_path, SensorGridOptions.Sensors).to_vtkjs(
+        folder='data', name=simple_model.identifier)
 
 with col2_con:
     st_vtkjs(
@@ -93,30 +93,46 @@ col3.subheader('Analysis Models')
 
 st.checkbox("Select Faces", room.faces)
 
-st.text_input("EPW Url")
+epw_data = st.file_uploader("EPW File", type=['epw'], key='epw_data')
+if epw_data:
+    epw_file = pathlib.Path(f'./data/{epw_data.name}')
+    st.write(epw_file)
+    # epw_file.parent.mkdir(parents=True, exist_ok=True)
+    # epw_file.write_bytes(epw_data.read())
+epw_obj = EPW(epw_file)
+wea_obj = Wea.from_epw_file(epw_file)
+wea_file = wea_obj.write(f'./data/weather_file.wea')
+st.write(wea_file)
+# .to_wea(file_path=epw_file)
 
 if run_simulation:
-    query.job_id = None
+    job_id = None
 
-    recipe = Recipe('ladybug-tools', 'daylight-factor',
+    recipe = Recipe('ladybug-tools', 'annual-daylight',
                     'latest', api_client)
-    new_job = NewJob(query.owner, query.project, recipe, client=api_client)
+    new_job = NewJob(owner, project, recipe, client=api_client)
     model_project_path = new_job.upload_artifact(
         pathlib.Path(model_path), 'streamlit-job')
-    # new_job.arguments = [
-    #     {'width': query.width, 'depth': query.depth,
-    #         'glazing-ration': query.glazing_ratio, 'model': model_project_path}
-    # ]
+    new_job.arguments = [
+        {'model': model_project_path,
+         'wea': wea_file,
+         'width': room_width, 
+         'depth': room_depth, 
+         'height': room_height,
+         'glazing-ration': wwr, 
+         'VLT': vlt, 
+         'SHGC': shgc}
+    ]
     job = new_job.create()
 
-    query.job_id = job.id
+    job_id = job.id
 
-    if query.job_id is not None and query.owner is not None and query.project is not None:
+    if job_id is not None and owner is not None and project is not None:
 
-        job = Job(query.owner, query.project, query.job_id, client=api_client)
+        job = Job(owner, project, job_id, client=api_client)
 
         st.write(
-            f'Checkout your job [here](https://app.pollination.cloud/projects/{query.owner}/{query.project}/jobs/{query.job_id})')
+            f'Checkout your job [here](https://app.pollination.cloud/{owner}/projects/{project}/jobs/{job_id})')
 
         if job.status.status in [
                 JobStatusEnum.pre_processing,
@@ -125,7 +141,7 @@ if run_simulation:
                 JobStatusEnum.unknown]:
             with st.spinner(text="Simulation in Progres..."):
                 st.warning(f'Simulation is {job.status.status.value}...')
-                st_autorefresh(interval=2000, limit=100)
+                # st_autorefresh(interval=2000, limit=100)
 
         elif job.status.status in [JobStatusEnum.failed, JobStatusEnum.cancelled]:
             st.warning(f'Simulation is {job.status.status.value}')
